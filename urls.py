e453de007ea856aa605943f265bf1d7d2f6102e2
chris <chris@collab.phub.net.cable.rogers.com>
2010-04-06 19:49:58 -0400

    added REST categories

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import *
+from contrib.django_restapi.model_resource import Collection
+from contrib.django_restapi.responder import *
 from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.contrib import admin
 from mainapp.feeds import LatestReports, LatestReportsByCity, LatestReportsByWard, LatestUpdatesByReport
-from mainapp.models import City
+from mainapp.models import City, ReportCategoryClass, ReportCategory
 import mainapp.views.cities as cities
 
 feeds = {
@@ -38,25 +40,25 @@ urlpatterns += patterns('mainapp.views.promotion',
 
 
 urlpatterns += patterns('mainapp.views.wards',
-    (r'^wards/(\d+)', 'show'),       
-    (r'^cities/(\d+)/wards/(\d+)', 'show_by_number'),       
-    
+    (r'^wards/(\d+)', 'show'),
+    (r'^cities/(\d+)/wards/(\d+)', 'show_by_number'),
+
 )
 
 urlpatterns += patterns('',
-    (r'^cities/(\d+)$', cities.show ),       
+    (r'^cities/(\d+)$', cities.show ),
     (r'^cities', cities.index),
 )
 
 urlpatterns += patterns( 'mainapp.views.reports.updates',
-    (r'^reports/updates/confirm/(\S+)', 'confirm'), 
-    (r'^reports/updates/create/', 'create'), 
+    (r'^reports/updates/confirm/(\S+)', 'confirm'),
+    (r'^reports/updates/create/', 'create'),
     (r'^reports/(\d+)/updates/', 'new'),
 )
 
 
 urlpatterns += patterns( 'mainapp.views.reports.subscribers',
-    (r'^reports/subscribers/confirm/(\S+)', 'confirm'), 
+    (r'^reports/subscribers/confirm/(\S+)', 'confirm'),
     (r'^reports/subscribers/unsubscribe/(\S+)', 'unsubscribe'),
     (r'^reports/subscribers/create/', 'create'),
     (r'^reports/(\d+)/subscribers', 'new'),
@@ -68,7 +70,7 @@ urlpatterns += patterns( 'mainapp.views.reports.flags',
 )
 
 urlpatterns += patterns('mainapp.views.reports.main',
-    (r'^reports/(\d+)$', 'show'),       
+    (r'^reports/(\d+)$', 'show'),
     (r'^reports/', 'new'),
 )
 
@@ -81,10 +83,31 @@ urlpatterns += patterns('mainapp.views.ajax',
     (r'^ajax/categories/(\d+)', 'category_desc'),
 )
 
+# REST Urls
+rest_format = r'(?P<format>\w+)'
+urlpatterns += patterns('mainapp.views.rest',
+   url(r'^rest/reports.%s$'%rest_format, 'reports_rest'),
+)
+
+urlpatterns += patterns('mainapp.views.rest',
+   url(r'^rest/report/create/$', 'report_create',name='create'),
+)
+
+# Categories REST -- should move this to its rest.py
+json_poll_resource = Collection(
+    queryset = ReportCategory.objects.all(),
+    expose_fields = ('id', 'name_en', 'name_fr'),
+    #permitted_methods = ('GET'),
+    responder = JSONResponder()
+)
+urlpatterns += patterns('mainapp.views.rest',
+   url(r'^rest/categories/(.*?)/?$', json_poll_resource),
+)
+
 #The following is used to serve up local media files like images
-if settings.LOCAL_DEV:
-    baseurlregex = r'^media/(?P<path>.*)$'
-    urlpatterns += patterns('',
-        (baseurlregex, 'django.views.static.serve',
-        {'document_root':  settings.MEDIA_ROOT}),
-    )
+#if settings.LOCAL_DEV:
+baseurlregex = r'^media/(?P<path>.*)$'
+urlpatterns += patterns('',
+   (baseurlregex, 'django.views.static.serve',
+   {'document_root':  settings.MEDIA_ROOT, 'show_indexes': True}),
+   )
